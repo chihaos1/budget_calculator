c4a6afe2dda1f13a46c76b68ea7426623b4a0c7a
Chi-Hao Sheng <chihaos0629@gmail.com>
2020-11-07 15:33:56 -0500

    Added docstrings for the budget calculator class and the retirement and apr methods

---
+++ budget_calculator.py
@@ -0,0 +1,45 @@
+class Budget_Calculator:
+    """Calculates credit score and annual percentage rate, provides graph
+       to break down budget allocation and plans for repaying loans/mortgages
+       and annual percentage rate (APR), and compares user's budget with the 
+       50-30-20 rule. 
+       
+       Attributes: 
+    
+    
+    """
+    def retirement(self):
+        """Calculates how much of monthly budget should be directed to retirement
+           savings in order to reach pension goal. The function requires user's
+           current age, retirement age, planned amount of pension, current total retirement saving,
+           and current monthly retirement saving.The user will be notified if the current monthly
+           saving is on track to meeting the goal. 
+        
+            Attributes:
+                current_age(int): the current age of user. 
+                retirement_age(int): the age the user plans to retire. 
+                pension(int): the planned amount of pension.  
+                current_saving(int): the current amount of pension saved. 
+                current_monthly_saving(int): the current amount of monthy saving directed to pension.      
+            
+            Returns:
+                A message that informs the user if his or her monthly retirement saving is on track to 
+                meeting the pension goal. If not, user will be notified on how much they should increase
+                to the monthly retirement saving. 
+        
+        """
+    
+    def apr(self):
+        """Calculates the annual percentage rate of loans. The function requires loan amount, interest rate, 
+           loan term, and total fees
+            
+            Attributes:
+                loan_amount(int): the total amount of loan
+                interest(int): the percentage of interest from loan 
+                loan_term(int): the number of years to pay off a loan
+                fees(int): the total fees for taking out the loan
+
+            Returns:
+                A message that informs user of the APR of the loan. 
+        """
+        
