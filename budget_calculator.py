50a74c9e0309d3cd358507288ca050a0d984a6e2
Thomas Marmo <talbertmarmo@gmail.com>
2020-11-19 13:18:11 -0500

    Small fix to spending_graph

--- budget_calculator.py
+++ budget_calculator.py
@@ -106,9 +106,8 @@ class Budget_Calculator:
         sizes = housing, food, transport, insurance, savings, retirement, recreation
         explode = (0,0,0,0,0.1,0.1,0)
         
-        fig1, ax1 = plt.subplots()
-        ax1.pie(sizes, explode = explode, labels = labels, autopct='%1.1f%%', shadow=True, startangle=90))
-        ax1.axis('equal')
+        plt.pie(sizes, explode = explode, labels = labels, autopct='%1.1f%%', shadow=True, startangle=90)
+        plt.axis('equal')
         plt.show()
         
 
