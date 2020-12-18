e64f69f8b07a8355785c39bebf4593d062f4d41f
Thomas Marmo <talbertmarmo@gmail.com>
2020-12-18 17:06:55 -0500

    Finished test_credit_score (passes)

--- test_calculator.py
+++ test_calculator.py
@@ -7,6 +7,7 @@ from loan_calculator import Loan_Calculator
 from budget_calculator import Budget_Calculator
 from loan_calculator import Loan_Calculator
 import pytest
+import random
 
 #Anthony
 
@@ -38,12 +39,19 @@ def test_credit_score():
     """ Tests whether the boundaries of the credit scores have been established.
         This should be no less than 300 and no higher than 850
     """
+    standard = Loan_Calculator(50000, 0.05, 500, 5, 200)
     # awful credit score
-    assert credit_score(1, 5000, 1, 5, 5) == 300
+    assert standard.credit_score(1, 5000, 1, 5, 0) == 300
     # perfect credit score
-    assert credit_score(0, 0, 50, 0, 4) == 850
+    assert standard.credit_score(0, 0, 50, 0, 4) == 850
     # average credit score
-    assert credit_score(20, 200, 20, 2, 2) == 565
+    assert standard.credit_score(20, 200, 20, 2, 2) == 565
+    # random credit score
+    assert 300 <= standard.credit_score(random.randrange(0,25),
+                                 random.randrange(0,5000),
+                                 random.randrange(0,100),
+                                 random.randrange(0,7),
+                                 random.randrange(0,7)) <= 850
 
 #Chi-Hao
 def test_retirement():
